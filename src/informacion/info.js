c67c4c6f27f5eb4b6e32ae7a50f6a343b163416c
Mostro Papi <ruizvaria814@gmail.com>
2024-07-16 23:43:41 -0500

    camibando

---
+++ src/informacion/info.js
@@ -0,0 +1,46 @@
+export const MUSIC = [
+    {
+        id: 0, name: "HotFuss", author: "The killers",  picture:"https://cdn.pixabay.com/photo/2023/10/18/15/30/statue-8324499_1280.jpg", music:[
+            {id: 0, name: "Jenrry Was a Friend of Mine"},
+            {id: 1, name: "Jenrry Was a Friend of Mine"},
+            {id: 2, name: "Jenrry Was a Friend of Mine"},    
+            {id: 3, name: "Jenrry Was a Friend of Mine"},
+            {id: 4, name: "Jenrry Was a Friend of Mine"},
+        ]
+    },
+    {
+        id: 1, name: "First Impressions of Earth", author: "The Strokes", picture:"https://cdn.pixabay.com/photo/2016/03/30/08/24/peacock-1290248_1280.jpg", music:[
+            {id: 0, name: "Jenrry Was a Friend of Mine"}    
+        ]
+    },
+    {
+        id: 2, name: "Inside InInside Out", author: "The Kooks", picture:"https://cdn.pixabay.com/photo/2024/05/09/17/24/shih-tzu-8751508_960_720.jpg", music:[
+            {id: 0, name: "Jenrry Was a Friend of Mine"}    
+        ]
+    },
+    {
+        id: 3, name: "Favourite Worst Nightmare", author: "Arctic Monkeys", picture:"https://cdn.pixabay.com/photo/2023/09/28/21/22/highland-cattle-8282564_960_720.jpg", music:[
+            {id: 0, name: "Jenrry Was a Friend of Mine"}    
+        ]
+    },
+    {
+        id: 4, name: "Up All Night", author: "Razorlight", picture:"https://cdn.pixabay.com/photo/2024/02/15/15/46/cat-8575641_960_720.jpg", music:[
+            {id: 0, name: "Jenrry Was a Friend of Mine"}    
+        ]
+    },
+    {
+        id: 5, name: "Phrazes for the Young", author: "Julian Casablancas", picture:"https://cdn.pixabay.com/photo/2019/11/08/11/56/kitten-4611189_1280.jpg", music:[
+            {id: 0, name: "Jenrry Was a Friend of Mine"}    
+        ]
+    },
+    {
+        id: 6, name: "Franz Ferdinand", author: "Franz Ferdinand", picture:"https://cdn.pixabay.com/photo/2024/01/20/06/50/woman-8520431_1280.png", music:[
+            {id: 0, name: "Jenrry Was a Friend of Mine"}    
+        ]
+    },
+    {
+        id: 7, name: "Get Born", author: "Jet", picture:"https://cdn.pixabay.com/photo/2024/02/20/12/28/bird-8585426_1280.jpg", music:[
+            {id: 0, name: "Jenrry Was a Friend of Mine"}    
+        ]
+    },
+]
